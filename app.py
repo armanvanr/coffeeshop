ef73a6a42d15cfc586b99c2214632724f3ca2cdc
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-06-21 20:47:03 +0700

    user(update), menu(show, update, search) OK

--- app.py
+++ app.py
@@ -89,49 +89,116 @@ class Balance_Record(db.Model):
 
 @app.get("/")
 def welcome():
-    return {"message": "Welcome to Coffee Shop API"}
+    return {"success": True, "message": "Welcome to Coffee Shop API", "data": {}}
 
 
 @app.get("/users")
 def get_users():
-    data = [
+    users = [
         {
             "name": user.name,
             "id": user.id,
             "balance": user.balance,
             "email": user.email,
             "role": user.role,
+            "password": user.password,
         }
         for user in User.query.all()
     ]
-    return {"message": data}, 200
+    return {"success": True, "message": "Data found", "data": {"users": users}}, 200
 
 
+# register a new user
 @app.post("/user")
-def register():
+def add_user():
     data = request.get_json()
     user = User.query.filter_by(email=data["email"]).first()
     if user:
-        return {"message": "Email already exists"}, 400
+        return {"success": False, "message": "Email already exists", "data": {}}, 400
+    hash_pw = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
     new_user = User(
         name=data["name"],
         email=data["email"],
-        password=bcrypt.generate_password_hash(data["password"]).decode("utf-8"),
+        password=hash_pw,
         role=data["role"],
     )
     db.session.add(new_user)
     db.session.commit()
-    return {"message": "Account successfully created"}, 201
+    return {"success": True, "message": "Account successfully created", "data": {}}, 201
 
 
+# change name or password
+@app.put("/user/<int:id>")
+def update_user(id):
+    data = request.get_json()
+    user = User.query.get(id)
+    if not user:
+        return {"success": False, "message": "Data not found", "data": {}}, 404
+    user.name = data.get("name", user.name)
+    if "new_password" in data.keys():
+        is_valid = bcrypt.check_password_hash(user.password, data["old_password"])
+        if is_valid:
+            hash_pw = bcrypt.generate_password_hash(data["new_password"]).decode(
+                "utf-8"
+            )
+            user.password = hash_pw
+    db.session.commit()
+    return {"success": True, "message": "Account data updated", "data": {}}, 200
+
+
+# show all in-stock menus
+@app.get("/menus")
+def get_menus():
+    drinks = [
+        {
+            "name": menu.name,
+            "id": menu.id,
+            "img_url": menu.img_url,
+        }
+        for menu in Menu.query.filter_by(category="drinks")
+        if menu.stock > 0
+    ]
+    foods = [
+        {
+            "name": menu.name,
+            "id": menu.id,
+            "img_url": menu.img_url,
+        }
+        for menu in Menu.query.filter_by(category="foods")
+        if menu.stock > 0
+    ]
+    return {
+        "success": True,
+        "message": "Data found",
+        "data": {"drinks": drinks, "foods": foods},
+    }, 200
+
+
+# show a menu details
+@app.get("/menu/<int:id>")
+def get_menu(id):
+    menu = Menu.query.get(id)
+    details = {
+        "name": menu.name,
+        "id": menu.id,
+        "img_url": menu.img_url,
+        "price": menu.price,
+        "description": menu.desc,
+        "stock": menu.stock,
+    }
+
+    return {
+        "success": True,
+        "message": "Data found",
+        "data": {"details": details},
+    }, 200
+
+
+# add a new menu
 @app.post("/menu")
 def add_menu():
     data = request.get_json()
-    menu = Menu.query.filter_by(
-        name=data["name"], desc=data["desc"], price=data["price"]
-    )
-    if menu:
-        return {"message": "Menu item already exists"}
+
     new_menu = Menu(
         name=data["name"],
         desc=data["desc"],
@@ -142,7 +209,54 @@ def add_menu():
     )
     db.session.add(new_menu)
     db.session.commit()
-    return {"message": "menu added"}
+    return {"success": True, "message": "menu added", "data": {}}, 201
+
+
+# update menu data
+@app.put("/menu/<int:id>")
+def update_menu(id):
+    data = request.get_json()
+    menu = Menu.query.get(id)
+    menu.name = data.get("name", menu.name)
+    menu.desc = data.get("desc", menu.desc)
+    menu.price = data.get("price", menu.price)
+    menu.img_url = data.get("img_url", menu.img_url)
+    db.session.commit()
+    return {"success": True, "message": "menu updated", "data": {}}, 200
+
+
+# update menu stock
+@app.put("/menu/stock/<int:id>")
+def update_menu_stock(id):
+    data = request.get_json()
+    menu = Menu.query.get(id)
+    menu.stock = data.get("stock", menu.stock)
+    db.session.commit()
+    return {"success": True, "message": "menu stock updated", "data": {}}, 200
+
+
+# search menu
+@app.get("/menusearch")
+def menu_search():
+    args = request.args
+    q = Menu.query
+    if "name" in args.keys():
+        q = q.filter(Menu.name.ilike(f"%{args['name']}%"))
+    menus = q.all()
+    results = [
+        {
+            "name": menu.name,
+            "id": menu.id,
+            "img_url": menu.img_url,
+        }
+        for menu in menus
+        if menu.stock > 0
+    ]
+    return {
+        "success": True,
+        "message": "Data found",
+        "data": {"results": results},
+    }, 200
 
 
 if __name__ == "__main__":
