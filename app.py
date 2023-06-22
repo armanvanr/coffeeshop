d2e5dffea9070e278df93194c4ea1605d1993851
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-06-22 16:21:26 +0700

    create member order OK

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from os import environ
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_httpauth import HTTPBasicAuth
+from datetime import datetime
 
 load_dotenv()
 db_username = environ["USER_NAME"]
@@ -17,6 +19,7 @@ app.config[
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
+auth = HTTPBasicAuth(app)
 
 
 class User(db.Model):
@@ -37,11 +40,13 @@ class Order(db.Model):
     __tablename__ = "order"
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, index=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    total_bill = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
+    customer_name = db.Column(db.String, nullable=False)
+    total_bill = db.Column(db.Integer, nullable=True)
     status = db.Column(db.String, nullable=False)
     created_date = db.Column(db.DateTime, nullable=False)
     completed_date = db.Column(db.DateTime, nullable=True)
+    order_items = db.relationship("Order_Items", backref="order")
 
     def __repr__(self):
         return f"<Order {self.id}>"
@@ -52,6 +57,7 @@ class Order_Items(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, index=True)
     order_id = db.Column(db.Integer, db.ForeignKey("order.id"), nullable=False)
     menu_id = db.Column(db.Integer, db.ForeignKey("menu.id"), nullable=False)
+    menu_name = db.Column(db.String, nullable=False)
     quantity = db.Column(db.SmallInteger, nullable=False)
 
     def __repr__(self):
@@ -87,12 +93,32 @@ class Balance_Record(db.Model):
         return f"<Transaction {self.id}>"
 
 
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(email=username).first()
+    # if not user:
+    #     return {"message": "Unauthorized"}, 401
+    # raise Exception({"message": "Unauthorized"}, 401)
+    is_valid = bcrypt.check_password_hash(user.password, password)
+    if user and is_valid:
+        return user
+    # else:
+    #     return {"message": "Forbidden"}, 403
+
+
+@auth.get_user_roles
+def get_user_roles(user):
+    roles = user.role
+    return roles
+
+
 @app.get("/")
 def welcome():
     return {"success": True, "message": "Welcome to Coffee Shop API", "data": {}}
 
 
 @app.get("/users")
+@auth.login_required(role="admin")
 def get_users():
     users = [
         {
@@ -128,12 +154,12 @@ def add_user():
 
 
 # change name or password
-@app.put("/user/<int:id>")
-def update_user(id):
+@app.put("/user/update")
+@auth.login_required()
+def update_user():
     data = request.get_json()
-    user = User.query.get(id)
-    if not user:
-        return {"success": False, "message": "Data not found", "data": {}}, 404
+    user = auth.current_user()
+
     user.name = data.get("name", user.name)
     if "new_password" in data.keys():
         is_valid = bcrypt.check_password_hash(user.password, data["old_password"])
@@ -143,12 +169,13 @@ def update_user(id):
             )
             user.password = hash_pw
     db.session.commit()
+    print(1)
     return {"success": True, "message": "Account data updated", "data": {}}, 200
 
 
-# show all in-stock menus
-@app.get("/menus")
-def get_menus():
+# show all in-stock menu
+@app.get("/menu")
+def get_all_menu():
     drinks = [
         {
             "name": menu.name,
@@ -176,6 +203,7 @@ def get_menus():
 
 # show a menu details
 @app.get("/menu/<int:id>")
+@auth.login_required(role=["cashier", "admin"], optional=True)
 def get_menu(id):
     menu = Menu.query.get(id)
     details = {
@@ -196,6 +224,7 @@ def get_menu(id):
 
 # add a new menu
 @app.post("/menu")
+@auth.login_required(role="admin")
 def add_menu():
     data = request.get_json()
 
@@ -214,6 +243,7 @@ def add_menu():
 
 # update menu data
 @app.put("/menu/<int:id>")
+@auth.login_required(role="admin")
 def update_menu(id):
     data = request.get_json()
     menu = Menu.query.get(id)
@@ -227,6 +257,7 @@ def update_menu(id):
 
 # update menu stock
 @app.put("/menu/stock/<int:id>")
+@auth.login_required(role=["admin", "cashier"])
 def update_menu_stock(id):
     data = request.get_json()
     menu = Menu.query.get(id)
@@ -237,19 +268,20 @@ def update_menu_stock(id):
 
 # search menu
 @app.get("/menusearch")
+@auth.login_required(role=["cashier", "member"], optional=True)
 def menu_search():
     args = request.args
     q = Menu.query
     if "name" in args.keys():
         q = q.filter(Menu.name.ilike(f"%{args['name']}%"))
-    menus = q.all()
+    menu_list = q.all()
     results = [
         {
             "name": menu.name,
             "id": menu.id,
             "img_url": menu.img_url,
         }
-        for menu in menus
+        for menu in menu_list
         if menu.stock > 0
     ]
     return {
@@ -257,6 +289,42 @@ def menu_search():
         "message": "Data found",
         "data": {"results": results},
     }, 200
+
+
+# create order
+@app.post("/order/member")
+@auth.login_required(role=["cashier", "member"])
+def create_order():
+    items = request.get_json()["order_items"]
+    print(items)
+    member = auth.current_user()
+    new_order = Order(
+        user_id=member.id,
+        customer_name=member.name,
+        status="created",
+        created_date=datetime.now(),
+    )
+    db.session.add(new_order)
+    db.session.commit()
+    total_bill = 0
+    for item in items:
+        menu = Menu.query.get(item["menu_id"])
+        new_item = Order_Items(
+            order_id=new_order.id,
+            menu_id=item["menu_id"],
+            menu_name=menu.name,
+            quantity=item["quantity"],
+        )
+        total_bill += menu.price * item["quantity"]
+        print("price and qty:",menu.price, item["quantity"])
+        new_order.order_items.append(new_item)
+    new_order.total_bill=total_bill
+    db.session.commit()    
+    return {
+        "success": True,
+        "message": "Order created",
+        "data": {},
+    }, 201
 
 
 if __name__ == "__main__":
